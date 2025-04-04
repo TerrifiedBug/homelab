269a689a80cedae7ff93d9e6546cb10d0cce426c
TerrifiedBug <dannyfeates@yahoo.co.uk>
2025-04-05 00:55:13 +0100

    Added CPU temp exporer

---
+++ portainer-stacks/prometheus-grafana/cpu-temp-prometheus-exporter/main.js
@@ -0,0 +1,24 @@
+//This is used so you can setup a listener in Prometheus to the server ip on port 8084 to extract the CPU temp. It's ran with /usr/bin/js cpu-temp-prometheus-exporter/main.js
+
+const http = require('http');
+const { exec } = require('child_process');
+const requestListener = function (req, res) {
+    res.writeHead(200);
+
+    exec('sensors|grep "high"|grep "Core"|cut -d "+" -f2|cut -d "." -f1|sort -nr|sed -n 1p', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+
+        res.end(`server_cpu_temp ${stdout}`);
+    });
+}
+
+const server = http.createServer(requestListener);
+server.listen(8084);
